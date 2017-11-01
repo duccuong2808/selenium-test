e144b3dcaafaf2bc013f1f461d48ae273b043c36
Duc Cuong <duccuong2808@gmail.com>
2017-11-01 17:12:44 +0700

    remove format html

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-rspec demo01_guru99.rb --format progress --format html --out reports/rspec.html --format RspecJunitFormatter --out reports/rspec.xml
+rspec demo01_guru99.rb --format progress --format RspecJunitFormatter --out reports/rspec.xml
